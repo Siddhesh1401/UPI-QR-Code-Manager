ae9ff50183c161c10a236a06de9a1eed67fb6958
Siddhesh1401 <siddhesh14012005@gmail.com>
2025-11-27 23:52:43 +0530

    Fix regular QR sharing to include message with image like split function

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for UPI QR Manager PWA
-const CACHE_NAME = 'upi-qr-manager-v20';
+const CACHE_NAME = 'upi-qr-manager-v21';
 
 // Detect base path
 const baseUrl = self.location.origin + self.registration.scope;
