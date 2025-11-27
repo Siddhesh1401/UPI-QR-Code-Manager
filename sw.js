235723a594935f046686ea6f0d9650b4fa5b1a7f
Siddhesh1401 <siddhesh14012005@gmail.com>
2025-11-28 00:26:47 +0530

    Simplify share messages - remove bold formatting and extra text, clean link format

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for UPI QR Manager PWA
-const CACHE_NAME = 'upi-qr-manager-v100';
+const CACHE_NAME = 'upi-qr-manager-v101';
 
 // Detect base path
 const baseUrl = self.location.origin + self.registration.scope;
