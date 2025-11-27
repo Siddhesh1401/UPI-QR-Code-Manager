5ed2ef5e19f449556e405b7f0311d2bdc03d1e03
Siddhesh1401 <siddhesh14012005@gmail.com>
2025-11-28 00:49:23 +0530

    Use tiny invisible dummy file for Android multi-file text workaround

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for UPI QR Manager PWA
-const CACHE_NAME = 'upi-qr-manager-v205';
+const CACHE_NAME = 'upi-qr-manager-v207';
 
 // Detect base path
 const baseUrl = self.location.origin + self.registration.scope;
